9fa294042a467ff6312ac0ad680cbdb975272993
Oleksandr Filippov <ofilippov@prograils.com>
2022-08-18 12:25:46 +0200

    add comments for initializer

--- ___FILEBASENAME___/Configurator/___FILEBASENAME___Initializer.swift
+++ ___FILEBASENAME___/Configurator/___FILEBASENAME___Initializer.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ___VARIABLE_productName___ModuleInitializer: NSObject {
+	//TODO: need create storyboard in View folder and add IBOutlet below as Module Initializer in this Storyboard
 	@IBOutlet private weak var ___VARIABLE_productName___ViewController: ___VARIABLE_productName___ViewController!
 
 	override func awakeFromNib() {
